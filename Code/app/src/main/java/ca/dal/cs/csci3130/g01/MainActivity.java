b38536df76855d806b16b218ded7ba59a747870d
Mazin Asim Abdu Al Raisi <mz871166@dal.ca>
2023-02-17 11:20:58 -0400

    Finished US3, tests to be completed

--- Code/app/src/main/java/ca/dal/cs/csci3130/g01/MainActivity.java
+++ Code/app/src/main/java/ca/dal/cs/csci3130/g01/MainActivity.java
@@ -2,7 +2,10 @@ package ca.dal.cs.csci3130.g01;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button registerPageButton = findViewById(R.id.registerPageButton);
+        registerPageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent movingToRegisterPageIntent = new Intent(MainActivity.this, RegisterPage.class);
+                MainActivity.this.startActivity(movingToRegisterPageIntent);
+            }
+        });
+
     }
 }
