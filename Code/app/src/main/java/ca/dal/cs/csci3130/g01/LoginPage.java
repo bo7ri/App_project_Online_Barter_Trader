5a81265e882f32b51d7ee2bf609cc587b251d8af
Jaifar Al-Musalhi <jf490907@dal.ca>
2023-02-17 20:45:02 -0400

    US1 and 2 completed and test remaining

---
+++ Code/app/src/main/java/ca/dal/cs/csci3130/g01/LoginPage.java
@@ -0,0 +1,82 @@
+package ca.dal.cs.csci3130.g01;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.security.Provider;
+
+public class LoginPage extends AppCompatActivity {
+
+    EditText username, password;
+    Button login;
+    //Firebase connection
+    FirebaseFirestore databaseInstance = FirebaseFirestore.getInstance();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        username = findViewById(R.id.Username);
+        password = findViewById(R.id.Password);
+        login = findViewById(R.id.Login);
+
+        // Creating the registering page button.
+//        Button registerPageButton = findViewById(R.id.Register);
+//        registerPageButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent movingToRegisterPageIntent = new Intent(LoginPage.this, RegisterPage.class);
+//                LoginPage.this.startActivity(movingToRegisterPageIntent);
+//            }
+//        });
+
+
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String userName = username.getText().toString().trim();
+                String pass = password.getText().toString().trim();
+
+                databaseInstance.collection("UserList").whereEqualTo("Username", userName).get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()){
+                         for (QueryDocumentSnapshot document : task.getResult()){
+                             String timp = document.get("Password").toString();
+                             if (pass.equals(timp)){
+                                 Toast.makeText(LoginPage.this, "Login Successful", Toast.LENGTH_SHORT).show();
+                                 //Intent to main page here
+//                                Intent moveToListPage = new Intent(LoginPage.this, ProvidersListings.class);
+//                                LoginPage.this.startActivity(moveToListPage);
+                             }
+                             else {
+                                 Toast.makeText(LoginPage.this, "Invalid Login", Toast.LENGTH_SHORT).show();
+                             }
+
+                         }
+
+
+                        }
+
+                    }
+                });
+
+            }
+        });
+    }
+    }
--- Code/app/src/main/java/ca/dal/cs/csci3130/g01/MainActivity.java
+++ Code/app/src/main/java/ca/dal/cs/csci3130/g01/MainActivity.java
@@ -2,13 +2,24 @@ package ca.dal.cs.csci3130.g01;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    Button logout;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
+
 }
