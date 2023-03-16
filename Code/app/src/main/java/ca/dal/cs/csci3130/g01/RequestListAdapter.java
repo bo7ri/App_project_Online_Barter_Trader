a10ef8d9128c55ac92dae893c8ec644b2fec9dbc
Mazin Asim Abdu Al Raisi <mz871166@dal.ca>
2023-03-16 15:15:41 -0300

    Created RequestList page and adapter.

---
+++ Code/app/src/main/java/ca/dal/cs/csci3130/g01/RequestListAdapter.java
@@ -0,0 +1,63 @@
+package ca.dal.cs.csci3130.g01;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Filter;
+import android.widget.Filterable;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.content.Context;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// Source used for help with creating Adapter: https://www.geeksforgeeks.org/how-to-create-dynamic-listview-in-android-using-firebase-firestore/ [Mar 16, 2023].
+
+public class RequestListAdapter extends ArrayAdapter<RequestItem> {
+
+    public RequestListAdapter(@NonNull Context context, ArrayList<RequestItem> requestItemArrayList) {
+        super(context, 0, requestItemArrayList);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View childView, @NonNull ViewGroup parent) {
+
+        // Inflater view.
+        View inflateRequestListView = childView;
+        if (inflateRequestListView == null) {
+            inflateRequestListView = LayoutInflater.from(getContext()).inflate(R.layout.request_list_item, parent, false);
+        }
+
+        // Get requestItem from ArrayList.
+        RequestItem requestItem = getItem(position);
+
+        // Get TextView of Product Title and Receiver Username.
+        TextView requestProductTitle = inflateRequestListView.findViewById(R.id.requestListItemProductTitleInput);
+        TextView requestReceiverName = inflateRequestListView.findViewById(R.id.requestListItemRequesterName);
+
+        // Setting product title and receiver username view.
+        requestProductTitle.setText(requestItem.getProductTitle());
+        requestReceiverName.setText(requestItem.getReceiverUsername());
+
+        // Adding onClick to the item.
+        inflateRequestListView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getContext(), "Item clicked is: " + requestItem.getProductTitle(), Toast.LENGTH_LONG).show();
+            }
+        });
+
+        // Returning the view.
+        return inflateRequestListView;
+
+    }
+
+}
--- Code/app/src/main/java/ca/dal/cs/csci3130/g01/RequestListPage.java
+++ Code/app/src/main/java/ca/dal/cs/csci3130/g01/RequestListPage.java
@@ -5,21 +5,33 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+// Source used for help with creating list page: https://www.geeksforgeeks.org/how-to-create-dynamic-listview-in-android-using-firebase-firestore/ [Mar 16, 2023].
 public class RequestListPage extends AppCompatActivity {
 
     // Initializing variables.
     private String username;
     private String usertype;
+    ListView requestItemListView;
+    ArrayList<RequestItem> requestItemArrayList;
+    FirebaseFirestore databaseInstance;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +39,42 @@ public class RequestListPage extends AppCompatActivity {
         // Loading in the layout.
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_requestlist_page);
+
+        // Getting intents.
+        String username = getIntent().getStringExtra("username");
+        String usertype = getIntent().getStringExtra("usertype");
+
+        // Initializing variables.
+        requestItemListView = findViewById(R.id.listViewOfRequests);
+        requestItemArrayList = new ArrayList<>();
+        databaseInstance = FirebaseFirestore.getInstance();
+
+        // Getting data from the database.
+        // Looping through documentSnapshot captured by the whereEqualTo method.
+        // Converting to RequestItem object before adding to arraylist.
+        // Create and set request list adapter.
+        databaseInstance.collection("RequestList").whereEqualTo("ProviderUsername", username).get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+            @Override
+            public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                if (!queryDocumentSnapshots.isEmpty()) {
+                    List<DocumentSnapshot> documentSnapshotList = queryDocumentSnapshots.getDocuments();
+                    for (DocumentSnapshot document : documentSnapshotList) {
+                        RequestItem requestItem = document.toObject(RequestItem.class);
+                        requestItemArrayList.add(requestItem);
+                    }
+                    RequestListAdapter requestListAdapter = new RequestListAdapter(RequestListPage.this, requestItemArrayList);
+                    requestItemListView.setAdapter(requestListAdapter);
+                } else {
+                    Toast.makeText(RequestListPage.this, "There is no request messages for you! Try again later!", Toast.LENGTH_LONG).show();
+                }
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            // Sending a toast message if failed to load from database.
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(RequestListPage.this, "Error loading from Firestore Firebase!", Toast.LENGTH_LONG).show();
+            }
+        });
 
     }
 
