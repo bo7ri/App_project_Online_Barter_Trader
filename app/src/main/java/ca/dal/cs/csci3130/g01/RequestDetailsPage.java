04dad5c255bcfa41379c67395341c31154483605
Mazin Asim Abdu Al Raisi <mz871166@dal.ca>
2023-03-28 18:09:53 -0300

    Implemented code to retreive request message after clicking decline.

--- app/src/main/java/ca/dal/cs/csci3130/g01/RequestDetailsPage.java
+++ app/src/main/java/ca/dal/cs/csci3130/g01/RequestDetailsPage.java
@@ -8,9 +8,14 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -26,6 +31,9 @@ public class RequestDetailsPage extends AppCompatActivity {
         // Loading in the layout.
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_requestdetails_page);
+
+        // Getting instance of database.
+        FirebaseFirestore cloudDatabase = FirebaseFirestore.getInstance();
 
         // Getting intents.
         String ReceiverUsername = getIntent().getStringExtra("ReceiverUsername");
@@ -68,6 +76,40 @@ public class RequestDetailsPage extends AppCompatActivity {
         requestDeclineButton.setOnClickListener(view -> {
 
             // To do: find and delete request from database.
+
+            // Finding request from the database.
+            cloudDatabase.collection("RequestList").whereEqualTo("ProductTitle", ProductTitle)
+                    .whereEqualTo("ProviderUsername", ProviderUsername).whereEqualTo("ReceiverUsername", ReceiverUsername)
+                    .whereEqualTo("RequestMessage", RequestMessage).get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<QuerySnapshot> task) {
+
+                            // Checks if task is successful.
+                            if (task.isSuccessful()) {
+
+                                // Loops through the documents found by the previous query.
+                                for (QueryDocumentSnapshot document : task.getResult()) {
+
+                                    // Getting and storing the appropriate information.
+                                    String documentProductTitle = document.get("ProductTitle").toString();
+                                    String documentReceiverUsername = document.get("ReceiverUsername").toString();
+                                    String documentProviderUsername = document.get("ProviderUsername").toString();
+                                    String documentRequestMessage = document.get("RequestMessage").toString();
+
+                                    // Error checking methods to see if clicked request is same as retrieved request.
+                                    if (documentProductTitle.equals(ProductTitle) && documentReceiverUsername.equals(ReceiverUsername)
+                                            && documentProviderUsername.equals(ProviderUsername) && documentRequestMessage.equals(RequestMessage)) {
+                                        Toast.makeText(RequestDetailsPage.this, "Found request!", Toast.LENGTH_LONG).show();
+                                    } else {
+                                        Toast.makeText(RequestDetailsPage.this, "Did not find request!", Toast.LENGTH_LONG).show();
+                                    }
+
+                                }
+
+                            }
+
+                        }
+                    });
 
             // Sending the user back to the request list page.
             Intent moveBackToRequestListPage = new Intent(RequestDetailsPage.this, RequestListPage.class);
